cd40cb2e8bc43b4b121d9bec04ca2f5743d078ab
loopyloopez <165974767+loopyloopez@users.noreply.github.com>
2025-02-03 14:29:30 -0600

    Add files via upload
    init commit

---
+++ word_finder/Main.py
@@ -0,0 +1,66 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+
+def getresults():
+    Word = input("Enter your word: ")
+
+    url = f'https://www.thesaurus.com/browse/{Word}'
+    url2 = f'https://promova.com/antonyms-of/{Word}'
+    url3 = f'https://www.merriam-webster.com/dictionary/{Word}'
+
+
+
+    response = requests.get(url)
+    html_content = response.text
+    response2 = requests.get(url2)
+    html_content2 = response2.text
+    response3 = requests.get(url3)
+    html_content3 = response3.text
+
+    # Create a BeautifulSoup object
+    soup = BeautifulSoup(html_content, 'html.parser')
+    soup2 = BeautifulSoup(html_content2, 'html.parser')
+    soup3 = BeautifulSoup(html_content3, 'html.parser')
+
+
+    syn = soup.find_all("a", class_="Bf5RRqL5MiAp4gB8wAZa",limit=4)
+    ant = soup2.find_all("p", class_="synonyms_list_section_text__dJHzI",limit=4)
+    pro = soup3.find(class_="prons-entries-list-inline")
+    pro = [x.text for x in pro][1]
+    worldhistory = soup3.find("p", class_="et").text
+    firstused = soup3.find("p", class_="ety-sl pb-3").text
+    example = soup3.find_all(class_="sub-content-thread ex-sent sents",limit=3)
+    example = [x.text for x in example if x.text != "\n"]
+
+
+
+
+
+    syn = [x.text for x in syn]
+    ant = [x.text for x in ant]
+
+
+
+
+
+
+
+
+    Results = requests.get(f'https://api.dictionaryapi.dev/api/v2/entries/en/{str(Word)}').json()
+
+    print(f'results for {Word} are: \n Def:{Results[0]["meanings"][0]["definitions"][0]["definition"]} \n part of speech: {Results[0]["meanings"][0]["partOfSpeech"]} \n synonyms:{syn}\n antonyms:{ant} \n pronounce:{pro} \n world history:{worldhistory} \n firstused: {firstused}')
+    amount = int(input(f"{len(example)} examples found how many would you like to see?"))
+    if amount > len(example):
+        getresults()
+
+    for x in range(0,int(amount)):
+        print(example[x])
+
+
+    print("\n\n")
+
+    getresults()
+
+getresults()
+
